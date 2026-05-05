6aae8d714f1b3e51b323f483e4a55ef592da95e6
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-05-05 23:14:33 +0700

    Theory Done Sectio-05

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go_the_complete_guide/structs
 
 go 1.26.2
+
+require github.com/oklog/ulid/v2 v2.1.1 // indirect
--- structs.go
+++ structs.go
@@ -2,52 +2,31 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"go_the_complete_guide/structs/user"
 )
 
-type User struct {
-	FirstName string
-	LastName  string
-	Birthdate string
-	CreatedAt time.Time
-}
-
-func (user *User) outputUserDetail() {
-	fmt.Printf("First Name: %s\n", user.FirstName)
-	fmt.Printf("Last Name: %s\n", user.LastName)
-	fmt.Printf("Birthdate: %s\n", user.Birthdate)
-	fmt.Printf("Created At: %s\n", user.CreatedAt)
-}
-
-func (user *User) clearUsername() {
-
-	user.FirstName = ""
-	user.LastName = ""
-}
-
-func NewUser(firstName, lastName, birthdate string) *User {
-	return &User{
-		FirstName: firstName,
-		LastName:  lastName,
-		Birthdate: birthdate,
-		CreatedAt: time.Now(),
-	}
-
-}
-
 func main() {
-	// userFirstName := getUserData("Please enter your first name: ")
-	// userLastName := getUserData("Please enter your last name: ")
-	// userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	user := NewUser("Burhanudin", "Rabbani", "11/14/2002")
+	newUser, err := user.New(userFirstName, userLastName, userBirthdate)
 
-	user.outputUserDetail()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	newUser.OutputUserDetail()
+	newAdmin := user.NewAdmin("xxxxxxx", "123455667")
+	// newAdmin.ClearUsername()
+
+	fmt.Println(newAdmin)
+
 }
 
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
 	var value string
-	fmt.Scan(&value)
+	fmt.Scanln(&value)
 	return value
 }
