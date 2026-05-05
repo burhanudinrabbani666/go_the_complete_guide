7e135972eea79613387179c811ee58541a964947
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-05-05 07:26:04 +0700

    Alternative return value syntax

--- invesment_calculator.go
+++ invesment_calculator.go
@@ -35,9 +35,9 @@ func printQuestion(question string) {
 	fmt.Print(question)
 }
 
-func calculateFutureValues(invesmentAmount, expectedReturnRate, years float64) (float64, float64) {
-	futureValue := invesmentAmount * math.Pow((1+expectedReturnRate/100), years)
-	realFeatureValue := futureValue / math.Pow(1+INFLATION_RATE/100, years)
+func calculateFutureValues(invesmentAmount, expectedReturnRate, years float64) (futureValue float64, realFeatureValue float64) {
+	futureValue = invesmentAmount * math.Pow((1+expectedReturnRate/100), years)
+	realFeatureValue = futureValue / math.Pow(1+INFLATION_RATE/100, years)
 
 	return futureValue, realFeatureValue
 }
